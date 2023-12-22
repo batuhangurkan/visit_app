9753b48c344d68d406dfab0c5b03b0ae6ea86dd9
Batuhan <batuhan108668@gmail.com>
2023-12-22 23:41:53 +0300

    update

--- lib/pages/date_page.dart
+++ lib/pages/date_page.dart
@@ -1,6 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_directions/google_maps_directions.dart';
+import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart';
 
 class DatePage extends StatefulWidget {
   const DatePage({super.key});
@@ -10,27 +16,96 @@ class DatePage extends StatefulWidget {
 }
 
 class _DatePageState extends State<DatePage> {
+  String locationMessage = "test";
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Konum servisi kapalı');
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Konum izni verilmedi - forever'); // Konum izni verilmedi - forever
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('Buluşma Sayfası',
-              style: GoogleFonts.poppins(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              )),
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                FontAwesomeIcons.bell,
-                color: Colors.black,
-              ),
-              onPressed: () {},
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              _getCurrentLocation().then((value) => (value) {
+                    print(value.latitude);
+                    print(value.longitude);
+                    setState(() {
+                      locationMessage =
+                          "Enlem: ${value.latitude} \nBoylam: ${value.longitude}";
+                    });
+                  });
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) => Container(
+                        height: 400,
+                        color: Colors.white,
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                              'Gitmek istediğiniz yeri seçin',
+                              style: GoogleFonts.poppins(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                          ],
+                        ),
+                      ));
+            },
+            icon: Icon(
+              FontAwesomeIcons.search,
+              color: Colors.black,
+            )),
+        title: Text('Yer Seç',
+            style: GoogleFonts.poppins(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            )),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              FontAwesomeIcons.bell,
+              color: Colors.black,
             ),
-          ],
-        ));
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
   }
 }
